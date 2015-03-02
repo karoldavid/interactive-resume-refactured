26fa85bcdc377ca13a5ebc9634610b4b77083b75
lorak <k.zysk@zoho.com>
2015-03-02 17:42:51 +0100

    add json objects work, projects, bio and education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,82 +1,92 @@
-var name = "Karol Zyskowski";
-var role = "Web Developer";
-var skills = ["awesomeness","programming","teaching","JS"];
-var mobile = "+49 179 780 966 4";
-var email = "k.zysk@zoho.com";
-var github = "@karoldavid";
-var twitter = "@karoldawid";
-var city = "Berlin";
-var WelcomeMsg = "Hi there!";
-var bioPic = "images/karol.png";
-
-var bio = {
-  "name" : name,
-  "role": role,
-  "contacts" : {
-    "mobile" : mobile,
-    "email" : email,
-    "github" : github,
-    "twitter" : twitter,
-    "location" : city
-  },
-  "WelcomeMsg": WelcomeMsg,
-  "bioPic" : bioPic,
-  "skills": skills
+var work = {
+  "jobs" : [
+    {
+      "employer" : "agileTournee",
+      "title" : "Event Manager",
+      "location" : "Berlin Germany",
+      "dates": "2011-2015",
+      "description" : "Event Management"
+    },
+    {
+      "employer" : "Google (Manpower)",
+      "title" : "Legal Removals Specialist",
+      "location" : "Dublin Ireland",
+      "dates": "2014",
+      "description" : "The Right to be forgotten project"
+    },
+    {
+      "employer" : "Constanza Macras | DorkyPark GmbH",
+      "title" : "Production Manager",
+      "location" : "Berlin Germany",
+      "dates": "2010-2011",
+      "description" : "Tour Performance Management"
+    }
+  ]
 };
 
+var projects = {
+   "project": [
+     {
+       "title" : "Responsive Website",
+       "dates" : "2015",
+       "description" : "http://www.susanne-fuelscher.de/" ,
+       "images" : ["images/one.png","images/two.png","images/three.png"]
+   },
+   {
+      "title" : "Responsive Website",
+      "dates" : "2015",
+      "description" : "http://www.susanne-fuelscher.de/" ,
+      "images" : ["images/one.png","images/two.png","images/three.png"]
+   }
+ ]
+};
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var bio = {
+  "name" : "Karol Zyskowski",
+  "role": "Web Developer",
+  "contacts" : {
+    "mobile" : "+49 179 780 966 4",
+    "email" : "k.zysk@zoho.com",
+    "github" : "@karoldavid",
+    "twitter" : "@karoldawid",
+    "location" : "Berlin, Germany"
+  },
+  "WelcomeMsg": "Hi there!",
+  "bioPic" : "images/karol.png",
+  "skills": ["awesomeness","programming","teaching","JS"]
+};
 
-var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.WelcomeMsg);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-
-//var formattedContact = HTMLcontactGeneric.replace("%data%", formattedMobile);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-
-
-//$("#header").prepend(formattedContact);
-
-$("#header").append(formattedWelcomeMsg);
-$("#header").append(formattedBioPic);
-
-$("#topContacts").prepend(formattedLocation);
-$("#topContacts").prepend(formattedTwitter);
-$("#topContacts").prepend(formattedGithub);
-$("#topContacts").prepend(formattedEmail);
-$("#topContacts").prepend(formattedMobile);
-
-$("#header").append(HTMLskillsStart);
-//$("#header").append(formattedSkills);
-
-for (var i = 0; i < bio.skills.length; i++) {
-  var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
-  $("#header").append(formattedSkills);
-}
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-// var work = {};
-// work.position = "Event Manager";
-// work.employer = "agileTournee";
-// work.years = 3;
-// work.city = "Berlin";
-//
-//
-// $("#workExperience").prepend(HTMLworkStart);
-// $("#workExperience").append(work["position"]);
-//
-// $("#projects").prepend(HTMLprojectStart);
-//
-// var education = {};
-// education["name"] = "Oberstufe Alwin-Lonke-Straße";
-// education["years"] = "1992-1995";
-// education["city"] = "Bremen";
-//
-// $("#education").prepend(HTMLschoolStart);
-// $("#education").append(education.name);
+var education = {
+  "schools" : [
+  {
+    "name" : "University Bremen",
+    "location" : "Bremen, Germany",
+    "degree" : "Master in Political Science",
+    "major" : ["Political Science", "French Studies"],
+    "year" : "2001-2007",
+    "url" : "http://www.uni-bremen.de/"
+  },
+  {
+    "name" : "Institut d'Etudes Politiques de Lille",
+    "city" : "Lille, France",
+    "degree" : "Certificat d'Etudes Politiques",
+    "major" : ["Political Science", "French Studies"],
+    "year" : "2004-2005",
+    "url" : "http://www.sciencespo-lille.eu/"
+  }
+],
+  "online-courses" : [
+    {
+      "title" : "DataWrangling with MongoDB",
+      "school" : "Udacity",
+      "dates" : "2014",
+      "url" : "https://www.udacity.com/course/ud032"
+    },
+    {
+      "title" : "Scientific Programming with Python",
+      "school" : "MIT",
+      "dates" : "2013",
+      "url" : "https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-0#.VPSQ6DVVK1E"
+    }
+]
+};
