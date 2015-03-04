70d64e37384a5e6e4bd6fda5131565fae744896c
lorak <k.zysk@zoho.com>
2015-03-04 11:56:34 +0100

    add collecting click location js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -138,4 +138,18 @@ function displayWork() {
     }
 }
 
+function inName(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    var names = oldName.split(" ");
+
+    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    names[1] = names[1].toUpperCase();
+    finalName = names.join(" ");
+    // Don't delete this line!
+    return finalName;
+};
+
+//$("#main").append(internationalizeButton);
+
 displayWork();
