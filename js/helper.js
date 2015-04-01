448b6dfa84d0a5e9c10790f2ce6882e6b6f08d2e
lorak <k.zysk@zoho.com>
2015-04-01 02:15:08 +0200

    confuse branches

--- js/helper.js
+++ js/helper.js
@@ -13,17 +13,17 @@ var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><span class="light-gray-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkEmployer = '<a href="#" target="_blank">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%" width="200">';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -36,21 +36,40 @@ var work = {
       "title" : "Legal Removals Specialist",
       "location" : "Dublin, Ireland",
       "dates": "2014",
-      "description" : "The right to be forgotten project"
+      "description" : "The right to be forgotten project",
+      "url" : "https://support.google.com/legal/answer/3110420?rd=1"
     },
     {
       "employer" : "Constanza Macras | DorkyPark GmbH",
       "title" : "Production Manager",
       "location" : "Berlin, Germany",
       "dates": "2010-2011",
-      "description" : "Tour Performance Management"
+      "description" : "Tour Performance Management",
+      "url" : "http://www.dorkypark.org/"
+    },
+    {
+      "employer" : "Marcus Evans",
+      "title" : "Marketing Research Executive",
+      "location" : "Berlin, Germany",
+      "dates": "2010",
+      "description" : "Event Marketing Research",
+      "url" : "http://www.marcusevans.com/"
+    },
+    {
+      "employer" : "wbpr GmbH",
+      "title" : "Junior...",
+      "location" : "Berlin, Germany",
+      "dates": "2009",
+      "description" : "PR & Event Management",
+      "url" : "http://www.wbpr.de/"
     },
     {
       "employer" : "AHK Polska - Wiadomosci Gospodarcze",
       "title" : "Freelance Writer",
       "location" : "Warsaw, Poland",
       "dates": "2008-2009",
-      "description" : "Conducted Interviews, wrote articles, planned four magazines"
+      "description" : "Conducted Interviews, wrote articles, planned four magazines",
+      "url" : "http://ahk.pl/pl/media/"
     },
     {
       "employer" : "Die Deutsche Kammerphilharmonie Bremen",
@@ -58,15 +77,17 @@ var work = {
       "location" : "Bremen, Germany",
       "dates" : "2006-2008",
       "description" : "Assisted preparing, organizing, and coordinating classical music concerts",
-      "images" : ["images/placeholder.jpg","images/placeholder.jpg","images/placeholder.jpg"]
+      "images" : ["images/placeholder.jpg"],
+      "url" : "http://www.kammerphilharmonie.com/"
     },
     {
       "employer" : "Doc en Stock",
-      "title" : "Journaliste stagiare",
+      "title" : "Journaliste Stagiare",
       "location" : "Paris, France",
       "dates" : "2005",
       "description" : "Casted and invited guests for TV debates, researched and wrote treatments for documentary films" ,
-      "images" : ["images/placeholder.jpg","images/placeholder.jpg","images/placeholder.jpg"]
+      "images" : ["images/placeholder.jpg"],
+      "url" : "http://www.docenstock.com/"
     },
   ]
 };
@@ -74,34 +95,43 @@ var work = {
 var projects = {
    "projects": [
      {
-        "title" : "Polish Movie Poster Gallery with posters from 1955 to 1989",
+        "title" : "Responsive Website",
         "dates" : "2015",
-        "description" : "http://karoldavid.github.io/movie-poster-blog/",
+        "description" : "Responsive Website for a martial arts school",
+        "url" : "http://karoldavid.github.io/chinese-boxing/",
+        "images" : ["images/logo.svg"]
+     },
+     {
+        "title" : "Movie Poster Gallery Blog",
+        "dates" : "2015",
+        "description" : "Polish Movie Poster Gallery Website with art works from 1955 to 1989",
+        "url" : "http://karoldavid.github.io/movie-poster-blog/",
         "images" : ["images/dziecko_rosemary.svg", "images/zabity_na_smierc.svg", "images/w_mroku_nocy.svg"]
      },
      {
-       "title" : "Responsive Website for an author and TV script writer",
+       "title" : "Responsive Website",
        "dates" : "2015",
-       "description" : "http://www.susanne-fuelscher.de/" ,
+       "description" : "Responsive Website for an author and TV script writer",
+       "url" : "http://www.susanne-fuelscher.de/",
        "images" : ["images/susanne-fuelscher_small.jpg"]
    },
    {
       "title" : "PR Event for IFA (International Consumer Electronics)",
       "dates" : "2009",
-      "description" : "Organized and coordinated event, planned and wrote parcours, invited media" ,
-      "images" : ["images/placeholder.jpg","images/placeholder.jpg","images/placeholder.jpg"]
+      "description" : "Organized and coordinated event, planned and wrote parcours, invited media",
+      "images" : ["images/placeholder.jpg"]
    },
    {
       "title" : "Nouvelle PAC de L'UE",
       "dates" : "2004",
       "description" : "Bourse de la Fondation Robert Bosch pour un stage au sein de la CFPJ de Paris",
-      "images" : ["images/placeholder.jpg","images/placeholder.jpg","images/placeholder.jpg"]
+      "images" : ["images/placeholder.jpg"]
    },
    {
       "title" : "Concours de L'Express",
       "dates" : "2004-2005",
       "description" : "3ieme place" ,
-      "images" : ["images/placeholder.jpg","images/placeholder.jpg","images/placeholder.jpg"]
+      "images" : ["images/placeholder.jpg"]
    }
  ]
 };
@@ -160,43 +190,50 @@ var education = {
     }
 ]
 };
-//
-// var formattedphone = HTMLphone.replace("%data%", bio.contacts.phone);
-// $("#topContacts").append(formattedphone);
-// var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-// $("#topContacts").append(formattedEmail);
-// var formattedGithub= HTMLgithub.replace("%data%", bio.contacts.github);
-// $("#topContacts").append(formattedGithub);
-// var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-// $("#topContacts").append(formattedTwitter);
-// var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-// $("#topContacts").append(formattedLocation);
 
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-$("#info").append(formattedBioPic);
 
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#title").prepend(formattedRole);
-
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-$("#title").prepend(formattedName);
-
-if ( bio.skills.length > 0 )
-{
-  $("#info").append(HTMLskillsStart);
-  for (skill in bio.skills) {
-    var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-    $("#skills").append(formattedSkill);
+bio.display = function() {
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  $("#title").prepend(formattedRole);
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  $("#title").prepend(formattedName);
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+  $("#info").append(formattedBioPic);
+  if ( bio.skills.length > 0 )
+  {
+    $("#info").append(HTMLskillsStart);
+    for (skill in bio.skills) {
+      var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+      $("#skills").append(formattedSkill);
     }
-}
+  }
+  var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg);
+  $("#info").append(formattedWelcomeMsg);
+  var formattedphone = HTMLphone.replace("%data%", bio.contacts.phone);
+  $("#footerContacts").append(formattedphone);
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  $("#footerContacts").append(formattedEmail);
+  var formattedGithub= HTMLgithub.replace("%data%", bio.contacts.github);
+  $("#footerContacts").append(formattedGithub);
+  var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+  $("#footerContacts").append(formattedTwitter);
+  var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+  $("#footerContacts").append(formattedLocation);
 
-var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg);
-$("#info").append(formattedWelcomeMsg);
+  // add urls to footerContacts zocial
+  $('#footerContacts li').each(function() {
+    var url = {"email" : "mailto:" + bio.contacts.email, "github" : "https://github.com/karoldavid?tab=repositories", "twitter" : "https://twitter.com/karoldawid"};
+    var a = $(this).find('a');
+    var social = this.id;
+    a.attr('href', url[social]);
+  });
+}
 
 work.display = function() {
   for (job in work.jobs) {
     $("#workExperience").append(HTMLworkStart);
     var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    formattedEmployer = formattedEmployer.replace("#", work.jobs[job].url);
     var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
     $(".work-entry:last").append(formattedEmployer + formattedTitle);
     var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
@@ -212,6 +249,7 @@ projects.display = function() {
   for (project in projects.projects) {
     $("#projects").append(HTMLprojectStart);
     var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    formattedProjectTitle = formattedProjectTitle.replace("#", projects.projects[project].url);
     $(".project-entry:last").append(formattedProjectTitle);
     var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
     $(".project-entry:last").append(formattedProjectDates);
@@ -256,17 +294,6 @@ education.display = function() {
   }
 }
 
-var formattedphone = HTMLphone.replace("%data%", bio.contacts.phone);
-$("#footerContacts").append(formattedphone);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-$("#footerContacts").append(formattedEmail);
-var formattedGithub= HTMLgithub.replace("%data%", bio.contacts.github);
-$("#footerContacts").append(formattedGithub);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-$("#footerContacts").append(formattedTwitter);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-$("#footerContacts").append(formattedLocation);
-
 function inName(oldName) {
     var finalName = oldName;
     // Your code goes here!
@@ -279,63 +306,11 @@ function inName(oldName) {
     return finalName;
 };
 
-//$("#main").append(internationalizeButton);
+// $("#main").append(internationalizeButton);
 
+bio.display();
 work.display();
-
 projects.display();
-
 education.display();
 
 $("#mapDiv").append(googleMap);
-
-// main nav with different colors on hover
-$('#drawer li').hover(function (event) {
-  var colors = ["#176bec", "#d8432e","#ffb500", "#009451"]; // vivid blue, strong red, (mostly) pure orange, dark cyan/ lime green
-  var current = $(this).index();// get index in collection of the clicked item
-  $(this).css('background', colors[current]);
-  $(this).css('transition', '250ms ease-in');
-  $(this).css('cursor', 'pointer');
-  },
-  function() {
-    $(this).css('background', '#252525'); // reset background color to initial color
-    $(this).css('transition', '250ms ease-out');
-});
-
-  // main nav sections fade in/ out on click
-  $("#drawer li").click(function (event) {
-    var navItems = [];
-    // build array with section ids
-    $('main .section').each( function(i,e) {
-     navItems.push(e.id);
-   });
-
-    var current = navItems[$(this).index()];
-
-    for (item in navItems) {
-      if (navItems[item] != current) {
-        $('#' + navItems[item]).hide(1000);// make sure that all inactive sections are hidden
-      }
-      $('#info').hide(1000);// hide bioPic and skills when one section is shown
-    }
-    var info = document.getElementById(current);
-    if (info.style.display != 'none') {
-      $('#info').show(1000);// show bioPic and skills when all sections are hidden
-    };
-
-    $('#' + current).toggle(1000);//show or hide clicked section
-
-  });
-
-//add url to footer contacts zocial
-$('#footerContacts li').each(function() {
-  var url = {"email" : "mailto:" + bio.contacts.email, "github" : "https://github.com/karoldavid?tab=repositories", "twitter" : "https://twitter.com/karoldawid"};
-  var a = $(this).find('a');
-  var social = this.id;
-  a.attr('href', url[social]);
-});
-
-//remove all empty href attributes
-$("a[href='#']").each(function() {
-  this.removeAttribute("href");
-});
