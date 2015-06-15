b5ccdf030ddcbcbc30f9395291f8266b6fcf2b90
lorak <k.zysk@zoho.com>
2015-06-15 17:21:38 +0200

    refactor interactive online resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,318 +1,209 @@
-var bio = {
-  "name" : "Karol Zyskowski",
-  "role": "Web Developer",
-  "welcomeMsg": "Once the genie is out of the bottle...",
-  "bioPic" : "images/karol.png",
-  "skills": ["Responsive Websites","Mobile Development", "Wordpress", "Python","JavaScript","PHP"]
-};
 
-var contact = {
-  "phone" : "+49 30 301 393 71",
-  "email" : "k.zysk@zoho.com",
-  "github" : {"name" : "@karoldavid", "url" : "https://github.com/karoldavid?tab=repositories"},
-  "twitter" : {"name" : "@karoldawid", "url" : "https://twitter.com/karoldawid"},
-  "blog" : {"name" : "", "url" : ""},
-  "location" : "Berlin, Germany"
-};
-
-var work = {
-  "jobs" : [
-    {
-      "employer" : "Freelance",
-      "title" : "Web Developer",
-      "location" : "Berlin, Germany",
-      "dates": "2015",
-      "description" : "Responsive Websites",
-      "url" : "http://karoldavid.github.io/"
+$(function() {
+  var model = {
+    init: function() {
+      if (!localStorage.notes) {
+        localStorage.resume = JSON.stringify({'bio': bio, 'contact': contact, 'work': work, 'projects': projects, 'education': education});
+      }
     },
-    {
-      "employer" : "agileTournée",
-      "title" : "Event Manager",
-      "location" : "Berlin, Germany",
-      "dates": "2011-2015",
-      "description" : "Event Management",
-      "url" : "http://www.agiletournee.com/"
-    },
-    {
-      "employer" : "Google (Manpower)",
-      "title" : "Legal Removals Specialist",
-      "location" : "Dublin, Ireland",
-      "dates": "2014",
-      "description" : "The right to be forgotten project",
-      "url" : "https://support.google.com/legal/answer/3110420?rd=1"
-    },
-    {
-      "employer" : "Constanza Macras | DorkyPark GmbH",
-      "title" : "Production Manager",
-      "location" : "Berlin, Germany",
-      "dates": "2010-2011",
-      "description" : "Tour Performance Management: 9 month, 7 countries, 30 shows",
-      "url" : "http://www.dorkypark.org/"
-    },
-    {
-      "employer" : "Marcus Evans",
-      "title" : "Marketing Research Executive",
-      "location" : "Berlin, Germany",
-      "dates": "2010",
-      "description" : "Event Marketing Research",
-      "url" : "http://www.marcusevans.com/"
-    },
-    {
-      "employer" : "wbpr GmbH",
-      "title" : "Junior PR Consultant",
-      "location" : "Berlin, Germany",
-      "dates": "2009",
-      "description" : "PR & Event Management",
-      "url" : "http://www.wbpr.de/"
-    },
-    {
-      "employer" : "AHK Polska - Wiadomosci Gospodarcze",
-      "title" : "Freelance Writer",
-      "location" : "Warsaw, Poland",
-      "dates": "2008-2009",
-      "description" : "Conducted Interviews, wrote articles, planned four magazines",
-      "url" : "http://ahk.pl/pl/media/"
-    },
-    {
-      "employer" : "Die Deutsche Kammerphilharmonie Bremen",
-      "title" : "Assistent Tourmanagement",
-      "location" : "Bremen, Germany",
-      "dates" : "2006-2008",
-      "description" : "Organized and coordinated classical music concerts",
-      "url" : "http://www.kammerphilharmonie.com/"
-    },
-    {
-      "employer" : "Doc en Stock",
-      "title" : "Journaliste Stagiare",
-      "location" : "Paris, France",
-      "dates" : "2005",
-      "description" : "Casted and invited guests for TV debates, researched and wrote treatments for documentary films" ,
-      "url" : "http://www.docenstock.com/"
+    get: function(obj) {
+      return JSON.parse(localStorage.resume)[obj];
     }
-  ]
-};
+  };
 
-var projects = {
-   "projects": [
-     {
-        "title" : "Work in Progress: Online Movie Poster Gallery",
-        "dates" : "2015",
-        "description" : "Polish Movie Poster Gallery Website with 120 photographs of original art works from 1955 to 1989",
-        "url" : "http://karoldavid.github.io/movie-poster-blog/",
-        "images" : ["images/dziecko_rosemary.svg", "images/zabity_na_smierc.svg", "images/w_mroku_nocy.svg"]
-     },
-     {
-        "title" : "Draft: Responsive Website",
-        "dates" : "2015",
-        "description" : "Responsive Website for a Martial Arts School",
-        "url" : "http://karoldavid.github.io/chinese-boxing-international/",
-        "images" : ["images/cbii.svg"]
-     },
-     {
-       "title" : "Responsive Website",
-       "dates" : "2015",
-       "description" : "Responsive Website for an author and TV script writer",
-       "url" : "http://www.susanne-fuelscher.de/",
-       "images" : ["images/susanne-fuelscher.jpg"]
-   },
-   {
-      "title" : "PR Event for IFA (International Consumer Electronics)",
-      "dates" : "2009",
-      "description" : "Organized and coordinated event, planned and wrote parcours, invited media",
-      "url" : "http://www.morgenpost.de/printarchiv/berlin/article1165716/Loecher-in-die-Technik-fragen.html",
-   },
-   {
-      "title" : "Nouvelle PAC de L'UE",
-      "dates" : "2004",
-      "description" : "Bourse de la Fondation Robert Bosch pour un stage au sein de la CFPJ de Paris",
-      "url" : "http://www.cfpj.com/",
-   },
-   {
-      "title" : "Concours de L'Express",
-      "dates" : "2004-2005",
-      "description" : "3ieme place",
-      "url" : "http://www.lexpress.fr/"
-   }
-  ]
-};
+  var octopus = {
+    init: function() {
+      model.init();
 
-var education = {
-  "schools" : [
-  {
-    "name" : "University Bremen",
-    "location" : "Bremen, Germany",
-    "degree" : "Master in Political Science",
-    "major" : ["Political Science"],
-    "dates" : "2001-2007",
-    "url" : "http://www.uni-bremen.de/"
-  },
-  {
-    "name" : "Institut d'Etudes Politiques de Lille",
-    "location" : "Lille, France",
-    "degree" : "Certificat d'Etudes Politiques",
-    "major" : ["Political Science", "French Studies"],
-    "dates" : "2004-2005",
-    "url" : "http://www.sciencespo-lille.eu/"
-  },
-  {
-    "name" : "GEOS Auckland Language Center",
-    "location" : "Auckland, New Zealand",
-    "degree" : "Certificate of Achievement",
-    "major" : ["English"],
-    "dates" : "2005",
-    "url" : "http://www.language-programs.net/languageschool/12/18/geos_auckland_language_centre_auckland.htm"
+      titleView.init();
+      skillsView.init();
+      contactView.init();
+
+      workView.init();
+      projectsView.init();
+      educationView.init();
+    },
+    getBio: function() {
+      return model.get('bio');
+    },
+    getContact: function() {
+      return model.get('contact');
+    },
+    getWork: function() {
+      return model.get('work');
+    },
+    getProjects: function() {
+      return model.get('projects');
+    },
+    getEducation: function() {
+      return model.get('education');
+    }
+  };
+
+  // Credit goes to: http://jsfiddle.net/tJk34/
+  function checkIfEmailInString(text) { 
+    var re = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+    return re.test(text);
   }
-],
-  "onlineClasses" : [
-    {
-      "title" : "Nanodegree Front-End Web Development",
-      "school" : "Udacity",
-      "dates" : "2015",
-      "description" : "New type of project-based credential. It is built with industry to master skills that employers truly seek in a Front-End Web Developer",
-      "url" : "https://www.udacity.com/course/nd001"
+
+  var titleView = {
+    init: function() {
+      this.titleNameElem = $('#title h1');
+      this.titleRoleElem  = $('#title span');
+      this.infoElem = $('#info');
+      this.imgElem = document.createElement('img');
+      this.welcomeElem = $('#info span');
+
+      titleView.render();
     },
-    {
-      "title" : "DataWrangling with MongoDB",
-      "school" : "Udacity",
-      "dates" : "2014",
-      "description" : "Gather & extract data from widely used data formats. Assess quality of data. Best practices for data cleaning. MongoDB",
-      "url" : "https://www.udacity.com/course/ud032"
+    render: function() {
+      this.titleNameElem.text(octopus.getBio().name);
+      this.titleRoleElem.text(octopus.getBio().role);
+
+      this.imgElem.src = octopus.getBio().bioPic;
+      this.imgElem.className = "biopic";
+      this.infoElem.prepend(this.imgElem);
+
+      this.welcomeElem.text(octopus.getBio().welcomeMsg);
+      this.infoElem.append(this.welcomeElem);
+    }
+  };
+
+  var skillsView = {
+    init: function() {
+      this.skillsElem = $('#skills');
+      skillsView.render();
     },
-    {
-      "title" : "Sales Force App Development",
-      "school" : "Udacity",
-      "dates" : "2014",
-      "description" : "Build powerful web & mobile apps & host them in the cloud",
-      "url" : "https://www.udacity.com/course/ud162"
+    render: function() {
+      var skills = octopus.getBio().skills;
+
+      for (var i = 0; i < skills.length; i++) {
+        var elem = document.createElement('li');
+        elem.textContent =  skills[i];
+        elem.className = 'flex-item light-gray-text';
+        this.skillsElem.append(elem);
+      }
+    }
+  };
+
+  var contactView = {
+    init: function() {
+      this.footerContactElem = $('#footerContacts');
+      this.zocialClasses = {"phone": "zocial-call", "email": "zocial-email", "twitter": "zocial-twitter", "github": "zocial-github", "location": "" };
+
+      contactView.render();
     },
-    {
-      "title" : "Introduction to Computer Science and Programming Using Python",
-      "school" : "MIT",
-      "dates" : "2013",
-      "description" : "https://verify.edx.org/cert/10cbffa98e364fbb997fd633dc6c2301",
-      "url" : "https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-0#.VR2O9Mvh5z0"
+    render: function() {
+      var contact = octopus.getContact();
+
+      for (var p in this.zocialClasses) {
+
+        var zocial = this.zocialClasses[p],
+            id = p,
+            text = contact[p].name || contact[p],
+            target = "";
+
+        if (contact[p].url) {
+          target = 'href="' + contact[p].url + '" target="_blank"';
+        } else if (checkIfEmailInString(contact[p])) {
+          target = 'href="mailto:' + contact['email'] + '" target="_self"';
+        }
+ 
+        var elem = '<li class="flex-item contact" id="'
+                   + id + '"><a ' + target + '><span class="'
+                   + zocial + ' soft-blue-text"></span><span class="gray-text">'
+                   + text + '</span></a></li>';
+
+        this.footerContactElem.append(elem);
+      }
     }
-  ]
-};
+  };
 
-bio.display = function() {
-  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-  $("#title").prepend(formattedRole);
-  var formattedName = HTMLheaderName.replace("%data%", bio.name);
-  $("#title").prepend(formattedName);
-  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-  $("#info").append(formattedBioPic);
-  if ( bio.skills.length > 0 )
-  {
-    $("#info").append(HTMLskillsStart);
-    for (skill in bio.skills) {
-      var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-      $("#skills").append(formattedSkill);
+  var workView = {
+    init: function() {
+      this.workElem = $('#workExperience');
+
+      workView.render();
+    },
+    render: function() {
+      var jobs = octopus.getWork().jobs;
+      
+      for (var job in jobs) {
+        var elem = '<div class="work-entry">'
+                   + '<a href="' + jobs[job].url + '" target="_blank">'
+                   + jobs[job].employer + '- ' + jobs[job].title + '</a>'
+                   + '<div class="date-text">' + jobs[job].dates + '</div>'
+                   + '<div class="location-text">' + jobs[job].location + '</div>'
+                   + '<p><br>' + jobs[job].description + '</p></div>';
+
+        this.workElem.append(elem);
+      }
     }
-  }
-  var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg);
-  $("#info").append(formattedWelcomeMsg);
-}
+  };
 
-contact.display = function() {
-  var formattedphone = HTMLphone.replace("%data%", contact.phone);
-  $("#footerContacts").append(formattedphone);
-  var formattedEmail = HTMLemail.replace("%data%", contact.email);
-  $("#footerContacts").append(formattedEmail);
-  var formattedGithub= HTMLgithub.replace("%data%", contact.github["name"]);
-  $("#footerContacts").append(formattedGithub);
-  var formattedTwitter = HTMLtwitter.replace("%data%", contact.twitter["name"]);
-  $("#footerContacts").append(formattedTwitter);
-  var formattedLocation = HTMLlocation.replace("%data%", contact.location);
-  $("#footerContacts").append(formattedLocation);
+  var projectsView = {
+    init: function() {
+      this.projectsElem = $('#projects');
 
-  //add email + urls to footerContacts zocial
-  $('#footerContacts li').each(function() {
-    var url = {"email" : "mailto:" + contact.email, "github": contact.github["url"], "twitter" : contact.twitter["url"]};
-    var a = $(this).find('a');
-    var social = this.id;
-    a.attr('href', url[social]);
-  });
-}
+      projectsView.render();
+    },
+    render: function() {
+      var projects = octopus.getProjects().projects;
 
-work.display = function() {
-  for (job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    formattedEmployer = formattedEmployer.replace("#", work.jobs[job].url);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    $(".work-entry:last").append(formattedEmployer + formattedTitle);
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    $(".work-entry:last").append(formattedDates);
-    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    $(".work-entry:last").append(formattedLocation);
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedDescription);
+      for (var project in projects) {
+        var imgElems = "";
+
+        for (var img in projects[project].images) {
+          imgElems += '<img src="' + projects[project].images[img] + '" width="200">';
+        }
+
+        var elem = '<div class="project-entry">'
+                 + '<a href="' + projects[project].url + '" target="_blank">'
+                 + projects[project].title + '</a>' + '<div class="date-text">'
+                 + projects[project].dates + '</div><p><br>'
+                 + projects[project].description + '</p>'
+                 + imgElems + '</div>';
+
+        this.projectsElem.append(elem);
+      }
     }
-}
+  };
 
-projects.display = function() {
-  for (project in projects.projects) {
-    $("#projects").append(HTMLprojectStart);
-    var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-    formattedProjectTitle = formattedProjectTitle.replace("#", projects.projects[project].url);
-    $(".project-entry:last").append(formattedProjectTitle);
-    var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-    $(".project-entry:last").append(formattedProjectDates);
-    var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-    $(".project-entry:last").append(formattedProjectDescription);
-    for (image in projects.projects[project].images) {
-      var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-      $(".project-entry:last").append(formattedImage);
+  var educationView = {
+    init: function() {
+      this.educationElem = $('#education');
+
+      educationView.render();
+    },
+    render: function() {
+      var schools = octopus.getEducation().schools;
+
+      for (var school in schools) {
+        var elem = '<div class="education-entry">'
+                   + '<a href="' + schools[school].url + '" target="_blank">'
+                   + schools[school].name + ' -- ' + schools[school].degree
+                   + '</a><div class="date-text">' + schools[school].dates + '</div>'
+                   + '<div class="location-text">' + schools[school].location + '</div>'
+                   + '<em><br>Major: ' + schools[school].major + '</em></div>';
+
+        this.educationElem.append(elem);
+      }
+    
+      this.educationElem.append('<h3>Online Classes</h3>');
+
+      var onlineClasses = octopus.getEducation().onlineClasses;
+
+      for (var c in onlineClasses) {
+        var elem = '<div class="education-entry">'
+                   + '<a href="' + onlineClasses[c].url + '" target="_blank">' + onlineClasses[c].title
+                   + ' - ' + onlineClasses[c].school + '</a>'
+                   + '<div class="date-text">' + onlineClasses[c].dates + '</div>'
+                   + '<p><br>' + onlineClasses[c].description + '</p></div>';
+        this.educationElem.append(elem);
+      }
     }
-  }
-}
+  };
 
-education.display = function() {
-  for (school in education.schools) {
-    $("#education").append(HTMLschoolStart);
-    var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
-    formattedSchoolName = formattedSchoolName.replace("#", education.schools[school].url);
-    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-    $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
-    var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-    $(".education-entry:last").append(formattedSchoolDates);
-    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-    $(".education-entry:last").append(formattedSchoolLocation);
-    var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
-    $(".education-entry:last").append(formattedSchoolMajor);
-  }
-  if (education.onlineClasses) {
-    $("#education").append(HTMLonlineClasses);
-    for (onlineClass in education.onlineClasses) {
-      $("#education").append(HTMLschoolStart);
-      var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineClasses[onlineClass].title);
-      formattedOnlineTitle = formattedOnlineTitle.replace("#", education.onlineClasses[onlineClass].url);
-      var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineClasses[onlineClass].school);
-      $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
-      var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineClasses[onlineClass].dates);
-      $(".education-entry:last").append(formattedOnlineDates);
-      var formattedOnlineDescription = HTMLonlineDescription.replace("%data%", education.onlineClasses[onlineClass].description);
-      $(".education-entry:last").append(formattedOnlineDescription);
-    }
-  }
-}
-
-function inName(oldName) {
-    var finalName = oldName;
-    var names = oldName.split(" ");
-    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
-    names[1] = names[1].toUpperCase();
-    finalName = names.join(" ");
-    return finalName;
-};
-
-// $("#main").append(internationalizeButton);
-
-bio.display();
-work.display();
-projects.display();
-education.display();
-contact.display();
-
-$("#mapDiv").append(googleMap);
+  octopus.init();
+  $("#mapDiv").append(googleMap);
+});
---
+++ js/resumeData.js
@@ -0,0 +1,196 @@
+var bio = {
+  "name" : "Karol Zyskowski",
+  "role": "Web Developer",
+  "welcomeMsg": "Once the genie is out of the bottle...",
+  "bioPic" : "images/karol.png",
+  "skills": ["Responsive Websites","Mobile Development", "Wordpress", "Python","JavaScript","PHP"]
+};
+
+var contact = {
+  "phone" : "+49 30 301 393 71",
+  "email" : "k.zysk@zoho.com",
+  "github" : {"name" : "@karoldavid", "url" : "https://github.com/karoldavid?tab=repositories"},
+  "twitter" : {"name" : "@karoldawid", "url" : "https://twitter.com/karoldawid"},
+  "blog" : {"name" : "", "url" : ""},
+  "location" : "Berlin, Germany"
+};
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "Freelance",
+      "title" : "Web Developer",
+      "location" : "Berlin, Germany",
+      "dates": "2015",
+      "description" : "Responsive Websites",
+      "url" : "http://karoldavid.github.io/"
+    },
+    {
+      "employer" : "agileTournée",
+      "title" : "Event Manager",
+      "location" : "Berlin, Germany",
+      "dates": "2011-2015",
+      "description" : "Event Management",
+      "url" : "http://www.agiletournee.com/"
+    },
+    {
+      "employer" : "Google (Manpower)",
+      "title" : "Legal Removals Specialist",
+      "location" : "Dublin, Ireland",
+      "dates": "2014",
+      "description" : "The right to be forgotten project",
+      "url" : "https://support.google.com/legal/answer/3110420?rd=1"
+    },
+    {
+      "employer" : "Constanza Macras | DorkyPark GmbH",
+      "title" : "Production Manager",
+      "location" : "Berlin, Germany",
+      "dates": "2010-2011",
+      "description" : "Tour Performance Management: 9 month, 7 countries, 30 shows",
+      "url" : "http://www.dorkypark.org/"
+    },
+    {
+      "employer" : "Marcus Evans",
+      "title" : "Marketing Research Executive",
+      "location" : "Berlin, Germany",
+      "dates": "2010",
+      "description" : "Event Marketing Research",
+      "url" : "http://www.marcusevans.com/"
+    },
+    {
+      "employer" : "wbpr GmbH",
+      "title" : "Junior PR Consultant",
+      "location" : "Berlin, Germany",
+      "dates": "2009",
+      "description" : "PR & Event Management",
+      "url" : "http://www.wbpr.de/"
+    },
+    {
+      "employer" : "AHK Polska - Wiadomosci Gospodarcze",
+      "title" : "Freelance Writer",
+      "location" : "Warsaw, Poland",
+      "dates": "2008-2009",
+      "description" : "Conducted Interviews, wrote articles, planned four magazines",
+      "url" : "http://ahk.pl/pl/media/"
+    },
+    {
+      "employer" : "Die Deutsche Kammerphilharmonie Bremen",
+      "title" : "Assistent Tourmanagement",
+      "location" : "Bremen, Germany",
+      "dates" : "2006-2008",
+      "description" : "Organized and coordinated classical music concerts",
+      "url" : "http://www.kammerphilharmonie.com/"
+    },
+    {
+      "employer" : "Doc en Stock",
+      "title" : "Journaliste Stagiare",
+      "location" : "Paris, France",
+      "dates" : "2005",
+      "description" : "Casted and invited guests for TV debates, researched and wrote treatments for documentary films" ,
+      "url" : "http://www.docenstock.com/"
+    }
+  ]
+};
+
+var projects = {
+   "projects": [
+     {
+        "title" : "Work in Progress: Online Movie Poster Gallery",
+        "dates" : "2015",
+        "description" : "Polish Movie Poster Gallery Website with 120 photographs of original art works from 1955 to 1989",
+        "url" : "http://karoldavid.github.io/movie-poster-blog/",
+        "images" : ["images/dziecko_rosemary.svg", "images/zabity_na_smierc.svg", "images/w_mroku_nocy.svg"]
+     },
+     {
+        "title" : "Draft: Responsive Website",
+        "dates" : "2015",
+        "description" : "Responsive Website for a Martial Arts School",
+        "url" : "http://karoldavid.github.io/chinese-boxing-international/",
+        "images" : ["images/cbii.svg"]
+     },
+     {
+       "title" : "Responsive Website",
+       "dates" : "2015",
+       "description" : "Responsive Website for an author and TV script writer",
+       "url" : "http://www.susanne-fuelscher.de/",
+       "images" : ["images/susanne-fuelscher.jpg"]
+   },
+   {
+      "title" : "PR Event for IFA (International Consumer Electronics)",
+      "dates" : "2009",
+      "description" : "Organized and coordinated event, planned and wrote parcours, invited media",
+      "url" : "http://www.morgenpost.de/printarchiv/berlin/article1165716/Loecher-in-die-Technik-fragen.html",
+   },
+   {
+      "title" : "Nouvelle PAC de L'UE",
+      "dates" : "2004",
+      "description" : "Bourse de la Fondation Robert Bosch pour un stage au sein de la CFPJ de Paris",
+      "url" : "http://www.cfpj.com/",
+   },
+   {
+      "title" : "Concours de L'Express",
+      "dates" : "2004-2005",
+      "description" : "3ieme place",
+      "url" : "http://www.lexpress.fr/"
+   }
+  ]
+};
+
+var education = {
+  "schools" : [
+  {
+    "name" : "University Bremen",
+    "location" : "Bremen, Germany",
+    "degree" : "Master in Political Science",
+    "major" : ["Political Science"],
+    "dates" : "2001-2007",
+    "url" : "http://www.uni-bremen.de/"
+  },
+  {
+    "name" : "Institut d'Etudes Politiques de Lille",
+    "location" : "Lille, France",
+    "degree" : "Certificat d'Etudes Politiques",
+    "major" : ["Political Science", "French Studies"],
+    "dates" : "2004-2005",
+    "url" : "http://www.sciencespo-lille.eu/"
+  },
+  {
+    "name" : "GEOS Auckland Language Center",
+    "location" : "Auckland, New Zealand",
+    "degree" : "Certificate of Achievement",
+    "major" : ["English"],
+    "dates" : "2005",
+    "url" : "http://www.language-programs.net/languageschool/12/18/geos_auckland_language_centre_auckland.htm"
+  }
+],
+  "onlineClasses" : [
+    {
+      "title" : "Nanodegree Front-End Web Development",
+      "school" : "Udacity",
+      "dates" : "2015",
+      "description" : "New type of project-based credential. It is built with industry to master skills that employers truly seek in a Front-End Web Developer",
+      "url" : "https://www.udacity.com/course/nd001"
+    },
+    {
+      "title" : "DataWrangling with MongoDB",
+      "school" : "Udacity",
+      "dates" : "2014",
+      "description" : "Gather & extract data from widely used data formats. Assess quality of data. Best practices for data cleaning. MongoDB",
+      "url" : "https://www.udacity.com/course/ud032"
+    },
+    {
+      "title" : "Sales Force App Development",
+      "school" : "Udacity",
+      "dates" : "2014",
+      "description" : "Build powerful web & mobile apps & host them in the cloud",
+      "url" : "https://www.udacity.com/course/ud162"
+    },
+    {
+      "title" : "Introduction to Computer Science and Programming Using Python",
+      "school" : "MIT",
+      "dates" : "2013",
+      "description" : "https://verify.edx.org/cert/10cbffa98e364fbb997fd633dc6c2301",
+      "url" : "https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-0#.VR2O9Mvh5z0"
+    }
+  ]
+};
